41bf8202c4d118aff453a8cda91cfeb5330db400
andot <mabingyao@gmail.com>
2020-01-23 12:53:18 +0800

    Added TagHeader for hprose 3.0

--- hprose_tags.c
+++ hprose_tags.c
@@ -13,7 +13,7 @@
  *                                                        *
  * hprose tags for pecl source file.                      *
  *                                                        *
- * LastModified: Mar 14, 2015                             *
+ * LastModified: Jan 23, 2020                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
@@ -21,52 +21,56 @@
 #include "hprose_tags.h"
 
 static zend_function_entry hprose_tags_methods[] = {
-    ZEND_FE_END
-};
+    ZEND_FE_END};
 
 #define DECLARE_TAG(name, value) \
     zend_declare_class_constant_stringl(hprose_tags_ce, ZEND_STRL(name), ZEND_STRL(value) TSRMLS_CC);
 
 HPROSE_CLASS_ENTRY(tags)
 
-HPROSE_STARTUP_FUNCTION(tags) {
+HPROSE_STARTUP_FUNCTION(tags)
+{
     HPROSE_REGISTER_CLASS("Hprose", "Tags", tags);
     /* Serialize Tags */
-    DECLARE_TAG("TagInteger",       "i")
-    DECLARE_TAG("TagLong",          "l")
-    DECLARE_TAG("TagDouble",        "d")
-    DECLARE_TAG("TagNull",          "n")
-    DECLARE_TAG("TagEmpty",         "e")
-    DECLARE_TAG("TagTrue",          "t")
-    DECLARE_TAG("TagFalse",         "f")
-    DECLARE_TAG("TagNaN",           "N")
-    DECLARE_TAG("TagInfinity",      "I")
-    DECLARE_TAG("TagDate",          "D")
-    DECLARE_TAG("TagTime",          "T")
-    DECLARE_TAG("TagUTC",           "Z")
-    DECLARE_TAG("TagBytes",         "b")
-    DECLARE_TAG("TagUTF8Char",      "u")
-    DECLARE_TAG("TagString",        "s")
-    DECLARE_TAG("TagGuid",          "g")
-    DECLARE_TAG("TagList",          "a")
-    DECLARE_TAG("TagMap",           "m")
-    DECLARE_TAG("TagClass",         "c")
-    DECLARE_TAG("TagObject",        "o")
-    DECLARE_TAG("TagRef",           "r")
+    DECLARE_TAG("TagInteger", "i")
+    DECLARE_TAG("TagLong", "l")
+    DECLARE_TAG("TagDouble", "d")
+    DECLARE_TAG("TagNull", "n")
+    DECLARE_TAG("TagEmpty", "e")
+    DECLARE_TAG("TagTrue", "t")
+    DECLARE_TAG("TagFalse", "f")
+    DECLARE_TAG("TagNaN", "N")
+    DECLARE_TAG("TagInfinity", "I")
+    DECLARE_TAG("TagDate", "D")
+    DECLARE_TAG("TagTime", "T")
+    DECLARE_TAG("TagUTC", "Z")
+    DECLARE_TAG("TagBytes", "b")
+    DECLARE_TAG("TagUTF8Char", "u")
+    DECLARE_TAG("TagString", "s")
+    DECLARE_TAG("TagGuid", "g")
+    DECLARE_TAG("TagList", "a")
+    DECLARE_TAG("TagMap", "m")
+    DECLARE_TAG("TagClass", "c")
+    DECLARE_TAG("TagObject", "o")
+    DECLARE_TAG("TagRef", "r")
     /* Serialize Marks */
-    DECLARE_TAG("TagPos",           "+")
-    DECLARE_TAG("TagNeg",           "-")
-    DECLARE_TAG("TagSemicolon",     ";")
-    DECLARE_TAG("TagOpenbrace",     "{")
-    DECLARE_TAG("TagClosebrace",    "}")
-    DECLARE_TAG("TagQuote",         "\"")
-    DECLARE_TAG("TagPoint",         ".")
+    DECLARE_TAG("TagPos", "+")
+    DECLARE_TAG("TagNeg", "-")
+    DECLARE_TAG("TagSemicolon", ";")
+    DECLARE_TAG("TagOpenbrace", "{")
+    DECLARE_TAG("TagClosebrace", "}")
+    DECLARE_TAG("TagQuote", "\"")
+    DECLARE_TAG("TagPoint", ".")
     /* Protocol Tags */
-    DECLARE_TAG("TagFunctions",     "F")
-    DECLARE_TAG("TagCall",          "C")
-    DECLARE_TAG("TagResult",        "R")
-    DECLARE_TAG("TagArgument",      "A")
-    DECLARE_TAG("TagError",         "E")
-    DECLARE_TAG("TagEnd",           "z")
+    DECLARE_TAG("TagHeader", "H")
+    DECLARE_TAG("TagCall", "C")
+    DECLARE_TAG("TagResult", "R")
+    DECLARE_TAG("TagError", "E")
+    DECLARE_TAG("TagEnd", "z")
+
+    /* Obsolete Protocol Tags */
+    DECLARE_TAG("TagFunctions", "F")
+    DECLARE_TAG("TagArgument", "A")
+
     return SUCCESS;
 }
--- hprose_tags.h
+++ hprose_tags.h
@@ -13,60 +13,63 @@
  *                                                        *
  * hprose tags for pecl header file.                      *
  *                                                        *
- * LastModified: Mar 10, 2015                             *
+ * LastModified: Jan 23, 2020                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
 
 #ifndef HPROSE_TAGS_H
-#define	HPROSE_TAGS_H
+#define HPROSE_TAGS_H
 
 #include "hprose_common.h"
 
 BEGIN_EXTERN_C()
 
 /* Serialize Tags */
-#define HPROSE_TAG_INTEGER      'i'
-#define HPROSE_TAG_LONG         'l'
-#define HPROSE_TAG_DOUBLE       'd'
-#define HPROSE_TAG_NULL         'n'
-#define HPROSE_TAG_EMPTY        'e'
-#define HPROSE_TAG_TRUE         't'
-#define HPROSE_TAG_FALSE        'f'
-#define HPROSE_TAG_NAN          'N'
-#define HPROSE_TAG_INFINITY     'I'
-#define HPROSE_TAG_DATE         'D'
-#define HPROSE_TAG_TIME         'T'
-#define HPROSE_TAG_UTC          'Z'
-#define HPROSE_TAG_BYTES        'b'
-#define HPROSE_TAG_UTF8CHAR     'u'
-#define HPROSE_TAG_STRING       's'
-#define HPROSE_TAG_GUID         'g'
-#define HPROSE_TAG_LIST         'a'
-#define HPROSE_TAG_MAP          'm'
-#define HPROSE_TAG_CLASS        'c'
-#define HPROSE_TAG_OBJECT       'o'
-#define HPROSE_TAG_REF          'r'
+#define HPROSE_TAG_INTEGER 'i'
+#define HPROSE_TAG_LONG 'l'
+#define HPROSE_TAG_DOUBLE 'd'
+#define HPROSE_TAG_NULL 'n'
+#define HPROSE_TAG_EMPTY 'e'
+#define HPROSE_TAG_TRUE 't'
+#define HPROSE_TAG_FALSE 'f'
+#define HPROSE_TAG_NAN 'N'
+#define HPROSE_TAG_INFINITY 'I'
+#define HPROSE_TAG_DATE 'D'
+#define HPROSE_TAG_TIME 'T'
+#define HPROSE_TAG_UTC 'Z'
+#define HPROSE_TAG_BYTES 'b'
+#define HPROSE_TAG_UTF8CHAR 'u'
+#define HPROSE_TAG_STRING 's'
+#define HPROSE_TAG_GUID 'g'
+#define HPROSE_TAG_LIST 'a'
+#define HPROSE_TAG_MAP 'm'
+#define HPROSE_TAG_CLASS 'c'
+#define HPROSE_TAG_OBJECT 'o'
+#define HPROSE_TAG_REF 'r'
 
 /* Serialize Marks */
-#define HPROSE_TAG_POS          '+'
-#define HPROSE_TAG_NEG          '-'
-#define HPROSE_TAG_SEMICOLON    ';'
-#define HPROSE_TAG_OPENBRACE    '{'
-#define HPROSE_TAG_CLOSEBRACE   '}'
-#define HPROSE_TAG_QUOTE        '"'
-#define HPROSE_TAG_POINT        '.'
+#define HPROSE_TAG_POS '+'
+#define HPROSE_TAG_NEG '-'
+#define HPROSE_TAG_SEMICOLON ';'
+#define HPROSE_TAG_OPENBRACE '{'
+#define HPROSE_TAG_CLOSEBRACE '}'
+#define HPROSE_TAG_QUOTE '"'
+#define HPROSE_TAG_POINT '.'
 
 /* Protocol Tags */
-#define HPROSE_TAG_FUNCTIONS    'F'
-#define HPROSE_TAG_CALL         'C'
-#define HPROSE_TAG_RESULT       'R'
-#define HPROSE_TAG_ARGUMENT     'A'
-#define HPROSE_TAG_ERROR        'E'
-#define HPROSE_TAG_END          'z'
+#define HPROSE_TAG_HEADER 'H'
+#define HPROSE_TAG_CALL 'C'
+#define HPROSE_TAG_RESULT 'R'
+#define HPROSE_TAG_ERROR 'E'
+#define HPROSE_TAG_END 'z'
+
+/* Obsolete Protocol Tags */
+#define HPROSE_TAG_FUNCTIONS 'F'
+#define HPROSE_TAG_ARGUMENT 'A'
 
 HPROSE_STARTUP_FUNCTION(tags);
 
 END_EXTERN_C()
 
-#endif	/* HPROSE_TAGS_H */
+#endif /* HPROSE_TAGS_H */
